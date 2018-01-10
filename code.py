e1be81060bf43f924e228cd9397872334e8d055a
Hoang Cuong <hoangcuong2011@gmail.com>
2018-01-10 16:13:11 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -1,37 +1,25 @@
 
 import os
-
+import copy
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data as mnist_input_data
 import numpy as np
 from sklearn import cluster
 from scipy.spatial import distance
 import pandas as pd
-
+from keras.utils import np_utils
 import gpflow as gpf
+from sklearn.metrics import f1_score
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+import sys
 
 
 class DataPlaceholders(object):
     def __init__(self):
-        self.ximage_flat = tf.placeholder(tf.float32, shape=[None, 28*28])
-        self.x_image_reshaped = tf.reshape(self.ximage_flat,[-1, 28, 28, 1], name="img_reshaped")
+        self.data = tf.placeholder(tf.float32)        
+        self.keep_prob = tf.placeholder(tf.float32)        
         self.label = tf.placeholder(tf.int32, shape=[None, 1], name="labels")
 
-
-def get_mnist():
-    mnist = mnist_input_data.read_data_sets(os.path.join(os.path.dirname(__file__), "mnist_data/"), one_hot=False, validation_size=0)
-
-    norm_data = lambda img_in: 2.*img_in - 1.
-    add_extra_dim = lambda x: x[:, np.newaxis]
-
-    return (norm_data(mnist.train.images), add_extra_dim(mnist.train.labels),
-            norm_data(mnist.validation.images), add_extra_dim(mnist.validation.labels),
-            norm_data(mnist.test.images), add_extra_dim(mnist.test.labels))
-
-
-def create_weight(shape, stddev=0.1, dtype=tf.float32):
-    inital = tf.truncated_normal(shape, dtype=dtype, stddev=stddev, name="weight")
-    return inital
 
 
 def create_bias(shape, initial_val=0.1, dtype=tf.float32):
@@ -39,51 +27,24 @@ def create_bias(shape, initial_val=0.1, dtype=tf.float32):
     return initial
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME', name="2dconv")
-
-
-def max_pool_2by2(x):
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME', name="max_poll2by2")
-
-
-def make_small_mnist_nn(x_placeholder, end_h=50):
-    image_size = 28
-
+def make_feedforward_nn(x_placeholder, keep_prob_placeholder, end_h=50):
+    
     with tf.name_scope("small_convnet"):
         with tf.name_scope("layer1"):
-            W1 = tf.get_variable("W_conv1", initializer=create_weight([5,5,1,32]))
-            b1 = tf.get_variable("b_1", initializer=create_weight([32]))
-
-            h1 = tf.nn.relu(conv2d(x_placeholder, W1) + b1)
-            h1_pooled = max_pool_2by2(h1)
-
+            W1 = tf.get_variable("W1", shape=[72, 512], initializer=tf.contrib.layers.xavier_initializer())
+            b1 = tf.get_variable("b1", initializer=create_bias([512]))
+            h1 = tf.nn.relu(tf.matmul(x_placeholder, W1) + b1)
         with tf.name_scope("layer2"):
-            num_channels_layer2 = 64
-            W2 = tf.get_variable("W_conv2", initializer=create_weight([5, 5, 32, num_channels_layer2]))
-            b2 = tf.get_variable("b_2", initializer=create_weight([num_channels_layer2]))
-
-            h2 = tf.nn.relu(conv2d(h1_pooled, W2) + b2)
-            h2_pooled = max_pool_2by2(h2)
-            layer_length3 = ((image_size/4) ** 2) * num_channels_layer2  # 2 max pools of stride of 2
-            h2_pooled_flat = tf.reshape(h2_pooled, [-1, int(layer_length3)])
-
-        with tf.name_scope("layer3"):
-
-            W3 = tf.get_variable("W3", initializer=create_weight([int(layer_length3), 1024]))
-            b3 = tf.get_variable("b3", initializer=create_bias([1024]))
-            h3 = tf.nn.relu(tf.matmul(h2_pooled_flat, W3) + b3)
-
-        with tf.name_scope("layer4"):
-            W4 = tf.get_variable("W4", initializer=create_weight([1024, end_h]))
-            b4 = tf.get_variable("b4", initializer=create_bias([end_h]))
-            h4 = tf.matmul(h3, W4) + b4
-
-    return h4
+            W2 = tf.get_variable("W2", shape=[512, end_h], initializer=tf.contrib.layers.xavier_initializer())
+            b2 = tf.get_variable("b2", initializer=create_bias([end_h]))
+            h2 = tf.nn.relu(tf.matmul(h1, W2) + b2)
+            #h2 = (tf.matmul(h1, W2) + b2)
+            #h4_drop = tf.nn.dropout(h4, keep_prob_placeholder)
+    return h2
 
 
 def suggest_good_intial_inducing_points(phs: DataPlaceholders, x_data, h, tf_session, num_inducing):
-    h_data = tf_session.run(h, feed_dict={phs.ximage_flat: x_data})
+    h_data = tf_session.run(h, feed_dict={phs.data: x_data, phs.keep_prob: 1.0})
     kmeans = cluster.MiniBatchKMeans(n_clusters=num_inducing, batch_size=num_inducing*10)
     kmeans.fit(h_data)
     new_inducing = kmeans.cluster_centers_
@@ -91,12 +52,48 @@ def suggest_good_intial_inducing_points(phs: DataPlaceholders, x_data, h, tf_ses
 
 
 def suggest_sensible_lengthscale(phs: DataPlaceholders, x_data, h, tf_session):
-    h_data = tf_session.run(h, feed_dict={phs.ximage_flat: x_data})
+    h_data = tf_session.run(h, feed_dict={phs.data: x_data, phs.keep_prob: 1.0})
     lengthscale = np.mean(distance.pdist(h_data, 'euclidean'))
     return lengthscale
 
 
-def main():
+
+def standardize_data(X_train, X_test, X_valid):
+    unique_X_train = np.unique(X_train, axis=0)
+    X_mean = np.mean(unique_X_train, axis=0)
+    #print(X_mean)
+    X_std = np.std(unique_X_train, axis=0)+0.0000001 #a small noise
+    #print(X_std)
+    X_train -= X_mean
+    X_train /= X_std
+    X_test -= X_mean
+    X_test /= X_std
+    X_valid -= X_mean
+    X_valid /= X_std
+
+    return X_train, X_test, X_valid
+
+def compute_scores(flat_true, flat_pred):
+    f1_bad, f1_good = f1_score(flat_true, flat_pred, average=None, pos_label=None)
+    print("F1-BAD: ", f1_bad, "F1-OK: ", f1_good)
+    print("F1-score multiplied: ", f1_bad * f1_good)
+
+def resampleFile():
+    filename = open("train.revised", "w")
+    file = open("train", "r")
+    for x in file:
+        x = x.strip()
+        filename.write(x+"\n")
+        if x.endswith(",0"):
+            #filename.write(x+"\n")
+            filename.write(x+"\n")
+    filename.close()
+    file.close()
+
+
+
+def main(number_epoch):
+    
     """
     Simple demonstration of how you can put a GP on top of a NN and train the whole system end-to-end in GPflow-1.0.
 
@@ -109,37 +106,54 @@ def main():
     appropriate variable initialisation method.
     """
     # ## We load in the MNIST data. We will create a validation set but will not use it in this simple example.
-    x_train, y_train, x_val, y_val, x_test, y_test = get_mnist()
-    rng = np.random.RandomState(100)
-    train_permute = rng.permutation(x_train.shape[0])
-    x_train, y_train = x_train[train_permute, :], y_train[train_permute, :]
+    
+    dataset = np.loadtxt("test", delimiter=",")
+    x_test = dataset[:,0:72]
+    y_test = dataset[:,72].reshape(-1,1)
+    #print(x_test[20])
 
-    # ## We set up a TensorFlow Graph and a Session linked to this.
+    dataset = np.loadtxt("dev", delimiter=",")
+    x_valid = dataset[:,0:72]
+    y_valid = dataset[:,72].reshape(-1,1)
+    #resampleFile()
+    dataset = np.loadtxt("train.revised", delimiter=",")
+    x_train = dataset[:,0:72]
+    y_train = dataset[:,72].reshape(-1,1)
+
+    
+    x_train_root = x_train
+    x_valid_root = x_valid
+    x_train, x_test, x_valid = standardize_data(copy.deepcopy(x_train_root), x_test, copy.deepcopy(x_valid_root))
+
+        # ## We set up a TensorFlow Graph and a Session linked to this.
     tf_graph = tf.Graph()
     tf_session = tf.Session(graph=tf_graph)
 
     # ## We have some settings for the model and its training which we will set up below.
-    num_h = 100
-    num_classes = 10
+    num_h = 17
+    num_classes = 2 #could be improved here
     num_inducing = 100
     minibatch_size = 250
 
     # ## We set up the NN part of the GP kernel. This needs to be put on the same graph
     with tf_graph.as_default():
         phs = DataPlaceholders()
-        nn_base = tf.make_template("sconvnet_kernel", make_small_mnist_nn, end_h=num_h)  # end h is the number of hidden
+        nn_base = tf.make_template("sconvnet_kernel", make_feedforward_nn, end_h=num_h)  # end h is the number of hidden
         # units at the end
 
-        h = nn_base(phs.x_image_reshaped)
+        h = nn_base(phs.data, phs.keep_prob)
         h = tf.cast(h, gpf.settings.tf_float)
 
         nn_vars = tf.global_variables()  # only nn variables exist up to now.
     tf_session.run(tf.variables_initializer(nn_vars))
 
+
+
     # ## We now set up the GP part. Instead of the usual X data it will get the data after being processed by the NN.
-    kernel = gpf.kernels.RBF(num_h)
-    likelihood = gpf.likelihoods.MultiClass(num_classes)
-    gp_model = gpf.models.SVGP(h, phs.label, kernel, likelihood, np.ones((num_inducing, num_h), gpf.settings.np_float),
+    with gpf.defer_build():
+        kernel = gpf.kernels.RBF(num_h)
+        likelihood = gpf.likelihoods.MultiClass(num_classes)
+        gp_model = gpf.models.SVGP(h, phs.label, kernel, likelihood, np.ones((num_inducing, num_h), gpf.settings.np_float),
                                num_latent=num_classes, whiten=False, minibatch_size=None, num_data=x_train.shape[0])
     # ^ so we say minibatch size is None to make sure we get DataHolder rather than minibatch data holder, which
     # does not allow us to give in tensors. But we will handle all our minibatching outside.
@@ -147,61 +161,97 @@ def main():
 
     # ## The initial lengthscales and inducing point locations are likely very bad. So we use heuristics for good
     # initial starting points and reset them at these values.
-    gp_model.Z.assign(suggest_good_intial_inducing_points(phs, x_train[:5000, :], h, tf_session, num_inducing))
-    gp_model.kern.lengthscales.assign(suggest_sensible_lengthscale(phs, x_train[:5000, :], h, tf_session) + np.zeros_like(gp_model.kern.lengthscales.read_value()))
+
+    gp_model.Z.assign(suggest_good_intial_inducing_points(phs, np.unique(x_train, axis=0)[:5000, :], 
+        h, tf_session, num_inducing), tf_session)
+    gp_model.kern.lengthscales.assign(suggest_sensible_lengthscale(phs, np.unique(x_train, axis=0)[:5000, :], h, tf_session) 
+        + np.zeros_like(gp_model.kern.lengthscales.read_value()), tf_session)
+
+   
+
     # ^ note that this assign should reapply the transform for us :). The zeros ND array exists to make sure
     # the lengthscales are the correct shape via  broadcasting
 
     # ## We create ops to measure the predictive log likelihood and the accuracy.
     with tf_graph.as_default():
         log_likelihood_predict = gp_model.likelihood.predict_density(*gp_model._build_predict(h), phs.label)
-        accuracy = tf.cast(tf.equal(tf.argmax(gp_model.likelihood.predict_mean_and_var(*gp_model._build_predict(h))[0], axis=1, output_type=tf.int32),
-                                         tf.squeeze(phs.label)), tf.float32)
+        outputs = tf.argmax(gp_model.likelihood.predict_mean_and_var(*gp_model._build_predict(h))[0], axis=1, output_type=tf.int32)
+        accuracy = tf.cast(tf.equal(outputs, tf.squeeze(phs.label)), tf.float32)
         avg_acc = tf.reduce_mean(accuracy)
         avg_ll = tf.reduce_mean(log_likelihood_predict)
 
         # ## we now create an optimiser and initialise its variables. Note that you could use a GPflow optimiser here
         # and this would now be done for you.
         all_vars_up_to_trainer = tf.global_variables()
-        optimiser = tf.train.AdamOptimizer()
+        optimiser = tf.train.AdamOptimizer(1e-4)
         print(tf.global_variables())
         minimise = optimiser.minimize(gp_model.objective)  # this should pick up all Trainable variables.
         adam_vars = list(set(tf.global_variables()) - set(all_vars_up_to_trainer))
         tf_session.run(tf.variables_initializer(adam_vars))
+        saver = tf.train.Saver()
 
 
 
     # ## We now go through a training loop where we optimise the NN and GP. we will print out the test results at
-    # regular intervals.
-    data_indx = 0
+    # regular intervals.    
     results = []
-    print("starting")
-    for i in range(6000):
-        indx_array = np.mod(np.arange(data_indx, data_indx + minibatch_size), x_train.shape[0])
-        data_indx += minibatch_size
+    np.random.seed(number_epoch) #I did this to make sure I have a different randomization
+    
+    
+    print("Epoch: ", number_epoch)
+    i = number_epoch
+    if 1==1:
+    #for i in range(2): #100 epochs
+        if i>0:
+            #load truoc
+            #print_tensors_in_checkpoint_file(file_name="model_at_epoch"+str(i-1)+".ckpt", tensor_name='', all_tensors=False)
 
-        fd = gp_model.feeds or {}
-        fd.update({
-            phs.ximage_flat: x_train[indx_array],
-            phs.label: y_train[indx_array]
-        })
-        _, loss_evd = tf_session.run([minimise, -gp_model.objective], feed_dict=fd)
+            saver.restore(tf_session, "model_at_epoch"+str(i-1)+".ckpt")
 
-        # Print progress every 50 steps.
-        if i % 50 == 0:
             fd = gp_model.feeds or {}
-            fd.update({phs.ximage_flat: x_test,
+            fd.update({phs.keep_prob: 1.0, phs.data: x_test,
                        phs.label: y_test})
-            accuracy_evald, log_like_evald = tf_session.run([avg_acc, avg_ll], feed_dict=fd)
-            print("Iteration {}: Loss is {}. \nTest set LL {}, Acc {}".format(i, loss_evd, log_like_evald,
-                                                                              accuracy_evald))
-            results.append(dict(step_no=i, loss=loss_evd, test_acc=accuracy_evald, test_ll=log_like_evald))
+            accuracy_evald, log_like_evald, outputs_evald = tf_session.run([avg_acc, avg_ll, outputs], feed_dict=fd)
+            #print("Epoch {}: Loss is {}. \nTest set LL {}, Acc {}, Outputs {}".format(i, loss_evd, log_like_evald,
+            #                                                                  accuracy_evald, outputs_evald))
+            outputs_evald = outputs_evald.reshape(-1, 1)
+            #print(outputs_evald)
+            print("Result from the previous epoch:")
+            compute_scores(y_test, outputs_evald)
+
+
+
+        shuffle = np.arange(y_train.size)
+        np.random.shuffle(shuffle)
+        print(shuffle)
+        x_train_shuffle = x_train[shuffle]
+        y_train_shuffle = y_train[shuffle]
+        data_indx = 0
+        while data_indx<y_train.size:
+            lastIndex = data_indx + minibatch_size
+            if lastIndex>=y_train.size:
+                lastIndex = y_train.size
+            indx_array = np.mod(np.arange(data_indx, lastIndex), x_train_shuffle.shape[0])
+            #print("array", indx_array)
+            data_indx += minibatch_size
+            #print(data_indx)fz
+            fd = gp_model.feeds or {}
+            fd.update({
+                phs.keep_prob: 1.0,
+                phs.data: x_train_shuffle[indx_array],
+                phs.label: y_train_shuffle[indx_array]
+                })
+            _, loss_evd = tf_session.run([minimise, -gp_model.objective], feed_dict=fd)            
+            # Print progress every 1 epoch.
+        save_path = saver.save(tf_session, "./model_at_epoch"+str(i)+".ckpt")        
+
+            #results.append(dict(step_no=i, loss=loss_evd, test_acc=accuracy_evald, test_ll=log_like_evald))
+
     print("Done!")
 
-    # ## We save the results for looking at them later.
-    df = pd.DataFrame(results)
-    df.to_pickle("gpdnn_mnist_train.pdpick.pdpick")
 
 
 if __name__ == '__main__':
-    main()
+    #for arg in sys.argv[1:]:
+    #    print(arg)
+    main(int(sys.argv[1:][0]))
